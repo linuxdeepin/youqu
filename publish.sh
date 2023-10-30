7043a72dafcffe547e0972e67a33029b538747f5
mikigo <huangmingqiang@uniontech.com>
2023-10-30 11:40:02 +0800

    fix: 优化了打包发布性能；
    Description:
    
    Log:

--- publish.sh
+++ publish.sh
@@ -6,18 +6,20 @@
 tmp_publish_dir_name=_youqu_publish_tmp_dir
 cd ..
 rm -rf ${tmp_publish_dir_name}
-mkdir -p ${tmp_publish_dir_name}/youqu
-cp -r deepin-autotest-framework/. ${tmp_publish_dir_name}/youqu/
+mkdir -p ${tmp_publish_dir_name}/youqu/apps/
+ls deepin-autotest-framework | grep -v apps | grep -v public | grep -v report | xargs -i cp -r deepin-autotest-framework/{} ${tmp_publish_dir_name}/youqu/
+cp deepin-autotest-framework/apps/__init__.py ${tmp_publish_dir_name}/youqu/apps/
 rm -rf ${tmp_publish_dir_name}/youqu/.idea
 rm -rf ${tmp_publish_dir_name}/youqu/.vscode
 rm -rf ${tmp_publish_dir_name}/youqu/.pytest_cache
 rm -rf ${tmp_publish_dir_name}/youqu/.reuse
-#rm -rf ${tmp_publish_dir_name}/youqu/.gitignore
 rm -rf ${tmp_publish_dir_name}/youqu/pyproject.toml
 rm -rf ${tmp_publish_dir_name}/youqu/publish.sh
-rm -rf ${tmp_publish_dir_name}/youqu/report
+rm -rf ${tmp_publish_dir_name}/youqu/deploy.sh
 rm -rf ${tmp_publish_dir_name}/youqu/site
 rm -rf ${tmp_publish_dir_name}/youqu/docs
+rm -rf ${tmp_publish_dir_name}/youqu/README.en.md
+rm -rf ${tmp_publish_dir_name}/youqu/LICENSE
 rm -rf ${tmp_publish_dir_name}/youqu/mkdocs.yml
 rm -rf ${tmp_publish_dir_name}/youqu/requirements*.txt
 rm -rf ${tmp_publish_dir_name}/youqu/apps/autotest*
