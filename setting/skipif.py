95bee32d615ac6af7f8ad6df8b13403edde467a4
mikigo <huangmingqiang@uniontech.com>
2023-12-07 13:53:37 +0800

    fix: 优化标签化管理支持判断系统版本跳过用例的相关文档；
    Description:
    
    Log:

--- setting/skipif.py
+++ setting/skipif.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 # _*_ coding:utf-8 _*_
-
 # SPDX-FileCopyrightText: 2023 UnionTech Software Technology Co., Ltd.
 # SPDX-License-Identifier: GPL-2.0-only
-# pylint: disable=C0114
+
+"""
+此配置文件用于标签化管理方案实现条件判断的配置项；
+在CSV文件中“跳过原因”列填入函数名和参数，即可实现条件判断；
+比如：
+在CSV文件中“跳过原因”列填入：skipif_platform-aarch64
+    函数名称为：skipif_platform，比如是此文件中定义了的函数；
+    参数为：aarch64，多个参数用 & 符号连接；
+    函数名与参数之间用 - （中横线）连接；
+"""
 import os
 
 from setting.globalconfig import GlobalConfig
