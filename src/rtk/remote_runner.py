ba71556146446116fa9333d9751e77f2d8e8250f
mikigo <huangmingqiang@uniontech.com>
2024-01-25 15:31:33 +0800

    fix: update
    Description:
    
    Log:

--- src/rtk/remote_runner.py
+++ src/rtk/remote_runner.py
@@ -333,10 +333,9 @@ class RemoteRunner:
 
         cmd.extend(pytest_cmd)
         cmd.append('"')
-
         cmd_str = " ".join(cmd)
-        logger.info(f"\n{cmd_str}\n")
         if self.default.get(Args.debug.value):
+            logger.info(f"\n{cmd_str}\n")
             logger.info("DEBUG 模式不执行用例!")
         else:
             system(cmd_str)
