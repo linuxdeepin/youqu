0ea97a1f4c9aebb279ed319b4089d1238f66e90a
mikigo <huangmingqiang@uniontech.com>
2024-01-30 11:05:21 +0800

    fix: 报告新增显示当前目录
    Description:
    
    Log:

--- src/plugins/allure_report_extend.py
+++ src/plugins/allure_report_extend.py
@@ -36,9 +36,24 @@ class AllureReportExtend:
             wf(_f, f"网络地址={GlobalConfig.USERNAME}@{GlobalConfig.HOST_IP}")
             wf(_f, f"系统信息={GlobalConfig.PRODUCT_INFO}")
             wf(_f, f"镜像版本={GlobalConfig.VERSION}")
+            wf(_f, f"目录={GlobalConfig.ROOT_DIR}")
 
             screen = Tk()
             wf(_f, f"分辨率={screen.winfo_screenwidth()}x{screen.winfo_screenheight()}")
+
+            _display = GlobalConfig.DisplayServer.wayland if GlobalConfig.IS_WAYLAND else GlobalConfig.DisplayServer.x11
+            wf(_f, f"显示协议={_display.title()}")
+
+            cpu_info = os.popen("cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq -c").read().replace(" ", "")
+            if not cpu_info:
+                cpu_info = os.popen("cat /proc/cpuinfo | grep Hardware").read()
+            wf(_f, f"CPU信息={cpu_info}")
+
+            mem_info = os.popen("cat /proc/meminfo | grep MemTotal").read()
+            wf(_f, f"内存信息={mem_info}")
+
+            os_info = os.popen("uname -a").read()
+            wf(_f, f"内核信息={os_info}")
 
             try:
                 language_code = DbusUtils(
@@ -49,17 +64,3 @@ class AllureReportExtend:
                 wf(_f, f"系统语言={GlobalConfig.LANGUAGE_INI.get(language_code, default=language_code)}")
             except Exception:
                 pass
-
-            _display = GlobalConfig.DisplayServer.wayland if GlobalConfig.IS_WAYLAND else GlobalConfig.DisplayServer.x11
-            wf(_f, f"显示协议={_display.title()}")
-
-            os_info = os.popen("uname -a").read()
-            wf(_f, f"内核信息={os_info}")
-
-            cpu_info = os.popen("cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq -c").read().replace(" ", "")
-            if not cpu_info:
-                cpu_info = os.popen("cat /proc/cpuinfo | grep Hardware").read()
-            wf(_f, f"CPU信息={cpu_info}")
-
-            mem_info = os.popen("cat /proc/meminfo | grep MemTotal").read()
-            wf(_f, f"内存信息={mem_info}")
