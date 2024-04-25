379bcf191340eb4ebbd4f6de02c3052f47722bc6
mikigo <huangmingqiang@uniontech.com>
2024-04-25 10:48:59 +0800

    fix: update docs
    Description:
    
    Log:

--- publish.sh
+++ publish.sh
@@ -6,27 +6,36 @@ tmp_publish_dir_name=_youqu_publish_tmp_dir
 cd ..
 rm -rf ${tmp_publish_dir_name}
 mkdir -p ${tmp_publish_dir_name}/youqu/apps/
-ls youqu | grep -v apps | grep -v public | grep -v report | xargs -i cp -r youqu/{} ${tmp_publish_dir_name}/youqu/
+ls youqu | grep -v apps | grep -v public | grep -v report| grep -v node_modules | xargs -i cp -r youqu/{} ${tmp_publish_dir_name}/youqu/
 cp youqu/apps/__init__.py ${tmp_publish_dir_name}/youqu/apps/
-rm -rf ${tmp_publish_dir_name}/youqu/.idea
-rm -rf ${tmp_publish_dir_name}/youqu/.vscode
-rm -rf ${tmp_publish_dir_name}/youqu/.pytest_cache
-rm -rf ${tmp_publish_dir_name}/youqu/.reuse
-rm -rf ${tmp_publish_dir_name}/youqu/pyproject.toml
-rm -rf ${tmp_publish_dir_name}/youqu/publish.sh
-rm -rf ${tmp_publish_dir_name}/youqu/deploy.sh
-rm -rf ${tmp_publish_dir_name}/youqu/site
-rm -rf ${tmp_publish_dir_name}/youqu/docs
-rm -rf ${tmp_publish_dir_name}/youqu/README.en.md
-rm -rf ${tmp_publish_dir_name}/youqu/CONTRIBUTING.md
-rm -rf ${tmp_publish_dir_name}/youqu/LICENSE
-rm -rf ${tmp_publish_dir_name}/youqu/RELEASE.md
-rm -rf ${tmp_publish_dir_name}/youqu/mkdocs.yml
-rm -rf ${tmp_publish_dir_name}/youqu/requirements*.txt
-rm -rf ${tmp_publish_dir_name}/youqu/apps/autotest*
-rm -rf ${tmp_publish_dir_name}/youqu/Pipfile
-rm -rf ${tmp_publish_dir_name}/youqu/Pipfile.lock
-rm -rf ${tmp_publish_dir_name}/youqu/ci_result.json
+
+ignores=(
+    .idea
+    .vscode
+    .pytest_cache
+    .reuse
+    pyproject.toml
+    publish.sh
+    deploy.sh
+    site
+    docs
+    README.en.md
+    CONTRIBUTING.md
+    LICENSE
+    RELEASE.md
+    mkdocs.yml
+    requirements*.txt
+    apps/autotest*
+    Pipfile
+    Pipfile.lock
+    ci_result.json
+    package.json
+    pnpm-lock.yaml
+)
+for i in ${ignores[*]}
+do
+    rm -rf rm -rf ${tmp_publish_dir_name}/youqu/${i}
+done
 cp youqu/pyproject.toml ${tmp_publish_dir_name}/
 cp youqu/src/startproject.py ${tmp_publish_dir_name}/youqu/
 cd ${tmp_publish_dir_name}/
