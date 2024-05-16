6fd7cc2b822b25c4d746c333ec6a71b280ba70e7
DarkLii <liguangming@uniontech.com>
2024-05-16 19:35:29 +0800

    fix: 新增skipif_not_xxx方法
    Description:
    
    Log:

--- setting/skipif.py
+++ setting/skipif.py
@@ -29,6 +29,13 @@ def skipif_platform(args: str):
     return False
 
 
+def skipif_not_platform(args: str):
+    """平台不跳过
+    skipif_not_platform-aarch64
+    """
+    return not skipif_platform(args)
+
+
 def skipif_xdg_type(args: str):
     """skipif wayland or x11
     skipif_xdg_type-wayland
@@ -63,6 +70,15 @@ def skipif_cpu_name(args: str):
     return False
 
 
+def skipif_not_cpu_name(args: str):
+    """skipif not cpu name
+    使用 sudo dmidecode -s system-product-name 查看机器的cpu型号
+    剔除中横线和&符号，比如：KLVV-W5821，标签记录为 KLVVW5821
+    skipif_not_cpu_name-KLVVW5821
+    """
+    return not skipif_cpu_name(args)
+
+
 def skipif_os_version(args: str):
     """
     系统版本跳过
@@ -73,6 +89,14 @@ def skipif_os_version(args: str):
         if key == GlobalConfig.version_cfg.get("MinorVersion"):
             return True
     return False
+
+
+def skipif_not_os_version(args: str):
+    """
+    系统版本不跳过
+    skipif_not_os_version-1060
+    """
+    return not skipif_os_version(args)
 
 
 if __name__ == '__main__':
